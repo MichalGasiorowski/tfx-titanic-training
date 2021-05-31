6daca1148d3d5927ab6c57ba809b32622409b9d5
Michal Gasiorowski <enkidupal@gmail.com>
2021-05-31 23:53:48 +0200

    tfx 0.30 install from pip, after pypi package release

--- install_tfx_0.30.0.sh
+++ install_tfx_0.30.0.sh
@@ -18,28 +18,29 @@
 # pip install -q -U -v --log /tmp/pip.log tfx==0.25.0 apache-beam[gcp]==2.25.0 ml-metadata==0.25.0 pyarrow==0.17.0 tensorflow==2.3.0 tensorflow-data-validation==0.25.0 tensorflow-metadata==0.25.0 tensorflow-model-analysis==0.25.0 tensorflow-serving-api==2.3.0 tensorflow-transform==0.25.0 tfx-bsl==0.25.0
 
 cat > requirements.txt << EOF
-pandas>1.2.4
+pandas>=1.2.4
 apache-beam[gcp]==2.28.0
 ml-metadata==0.29.0
 pyarrow==2.0.0
 numpy==1.19.5
 tensorflow==2.4.0
-tensorflow-data-validation==0.29.0
-tensorflow-metadata==0.29.0
-tensorflow-model-analysis==0.29.0
+tensorflow-data-validation==0.30.0
+tensorflow-metadata==0.30.0
+tensorflow-model-analysis==0.30.0
 tensorflow-serving-api==2.4.0
-tensorflow-transform==0.29.0
-tfx-bsl==0.29.0
+tensorflow-transform==0.30.0
+tfx-bsl==0.30.0
+tfx==0.30.0
 tensorflow-cloud
 tensorboard
 kfp==1.4.0
 EOF
 
-python -m pip install -U --user -r requirements.txt
+python -m pip install -r requirements.txt
 
 # tfx==0.30.0
 # install from github since there is no tfx==0.30.0 in pypi yet
-pip install https://github.com/tensorflow/tfx/archive/refs/heads/r0.30.0.zip
+#pip install https://github.com/tensorflow/tfx/archive/refs/heads/r0.30.0.zip
 
 # Install Skaffold
 curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64
