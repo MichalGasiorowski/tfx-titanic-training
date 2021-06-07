1ebe39e162441c9a16c36ae5fa17de5260e2c14e
Michal Gasiorowski <enkidupal@gmail.com>
2021-06-07 18:15:24 +0200

    tfx 1.0.0 install script update

---
+++ install_tfx_1.0.0.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Copyright 2021 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#            http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Install KFP and TFX SDKs
+
+# pip install -q -U -v --log /tmp/pip.log tfx==0.25.0 apache-beam[gcp]==2.25.0 ml-metadata==0.25.0 pyarrow==0.17.0 tensorflow==2.3.0 tensorflow-data-validation==0.25.0 tensorflow-metadata==0.25.0 tensorflow-model-analysis==0.25.0 tensorflow-serving-api==2.3.0 tensorflow-transform==0.25.0 tfx-bsl==0.25.0
+
+cat > requirements.txt << EOF
+pandas>=1.2.4
+apache-beam[gcp]==2.29.0
+ml-metadata==1.0.0
+pyarrow==2.0.0
+numpy==1.19.5
+tensorflow==2.5.0
+tensorflow-data-validation==1.0.0
+tensorflow-metadata==1.0.0
+tensorflow-model-analysis==0.31.0
+tensorflow-serving-api==2.5.1
+tensorflow-transform==1.0.0
+tfx-bsl==1.0.0
+tfx==1.0.0
+tensorflow-cloud
+tensorboard
+kfp==1.4.0
+EOF
+
+python -m pip install -r requirements.txt
+
+# tfx==0.30.0
+# install from github since there is no tfx==0.30.0 in pypi yet
+#pip install https://github.com/tensorflow/tfx/archive/refs/heads/r0.30.0.zip
+
+# Install Skaffold
+curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64
+chmod +x skaffold
+mv skaffold $HOME/.local/bin
+
+jupyter nbextension enable --py tensorflow_model_analysis
